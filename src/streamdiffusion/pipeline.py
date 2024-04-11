8d7819b3564fec915c1a5a79bc69f753df560d89
NoelEmmanuel <79769249+NoelEmmanuel@users.noreply.github.com>
2024-04-10 21:58:41 -0500

    N/A

--- src/streamdiffusion/pipeline.py
+++ src/streamdiffusion/pipeline.py
@@ -585,7 +585,7 @@ class StreamDiffusion:
         text_encoder_lora_scale = (
             cross_attention_kwargs.get("scale", None) if cross_attention_kwargs is not None else None
         )
-        prompt_embeds, negative_prompt_embeds = self.encode_prompt(
+        prompt_embeds, negative_prompt_embeds = self.pipe.encode_prompt(
             prompt,
             device,
             num_images_per_prompt,
