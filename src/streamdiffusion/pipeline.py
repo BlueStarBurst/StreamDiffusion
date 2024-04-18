2afb42d337d7b7bd7fd33ec1fcb3d473a31f73e3
NoelEmmanuel <79769249+NoelEmmanuel@users.noreply.github.com>
2024-04-17 21:34:44 -0500

    N/A

--- src/streamdiffusion/pipeline.py
+++ src/streamdiffusion/pipeline.py
@@ -270,7 +270,7 @@ class StreamDiffusion:
         # we do that before converting to dtype to avoid breaking in case we're using cpu_offload
         # and half precision
         mask = torch.nn.functional.interpolate(
-            mask, size=(height // self.vae_scale_factor, width // self.vae_scale_factor)
+            mask, size=(height // self.pipe.vae_scale_factor, width // self.pipe.vae_scale_factor)
         )
         mask = mask.to(device=device, dtype=dtype)
 
