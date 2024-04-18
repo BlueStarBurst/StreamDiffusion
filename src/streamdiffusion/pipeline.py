0b5fbf9e7737ea30591b1fdf89f5df39c169abac
NoelEmmanuel <79769249+NoelEmmanuel@users.noreply.github.com>
2024-04-17 22:02:38 -0500

    N/A

--- src/streamdiffusion/pipeline.py
+++ src/streamdiffusion/pipeline.py
@@ -42,7 +42,7 @@ class StreamDiffusion:
 
         self.latent_height = int(height // pipe.vae_scale_factor)
         self.latent_width = int(width // pipe.vae_scale_factor)
-        self.vae_scale_factor = pipe.vae_scale_factor
+        # self.vae_scale_factor = pipe.vae_scale_factor
 
         self.frame_bff_size = frame_buffer_size
         self.denoising_steps_num = len(t_index_list)
@@ -77,9 +77,9 @@ class StreamDiffusion:
         self.pipe = pipe
         
         self.vae_scale_factor = 2 ** (len(self.vae.config.block_out_channels) - 1)
-        self.image_processor = VaeImageProcessor(self.vae_scale_factor)
+        self.image_processor = VaeImageProcessor(pipe.vae_scale_factor)
         self.mask_processor = VaeImageProcessor(
-            vae_scale_factor=self.vae_scale_factor, do_normalize=False, do_binarize=True, do_convert_grayscale=True
+            vae_scale_factor=pipe.vae_scale_factor, do_normalize=False, do_binarize=True, do_convert_grayscale=True
         )
 
         self.scheduler = LCMScheduler.from_config(self.pipe.scheduler.config)
