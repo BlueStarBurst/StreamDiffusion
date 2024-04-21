60a93bd696a3d69aa34a924e623a5534d1aba970
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2024-04-21 12:29:44 -0500

    3d

--- src/streamdiffusion/pipeline.py
+++ src/streamdiffusion/pipeline.py
@@ -546,7 +546,12 @@ class StreamDiffusion:
                         self.alpha_prod_t_sqrt[1:] * x_0_pred_batch[:-1]
                     )
                 # latents = (1 - init_mask) * init_latents_proper + init_mask * latents
+                # mask = mask.unsqueeze(0)
+                
+                # duplicate a layer of the mask to be 3 dimensional
                 mask = mask.unsqueeze(0)
+                mask = mask.repeat(self.frame_bff_size, 1, 1, 1)
+                
                 print(mask.size(), prev_latent_batch.size())
                 self.x_t_latent_buffer = (1 - mask) * self.x_t_latent_buffer + mask * prev_latent_batch
             else:
