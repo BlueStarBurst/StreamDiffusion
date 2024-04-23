01d77f4574f53a734d7ea3872014473dcdd93aef
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2024-04-23 18:33:47 -0500

    a

--- src/streamdiffusion/pipeline.py
+++ src/streamdiffusion/pipeline.py
@@ -559,10 +559,10 @@ class StreamDiffusion:
                 
                 # expand mask dim 0 to 3 to match the size of the latents
                 
-                # remove the alpha channel from the x_t_latent
-                test = x_t_latent[:, 1:, :, :]                
+                # remove the alpha channel from the x_t_latent [4,4,64,64] to [3,4,64,64]
+                test = x_0_pred_batch[-1][:3]
                 
-                print("here",mask.size(), prev_latent_batch.size(), x_t_latent.size()) 
+                print("here",mask.size(), prev_latent_batch.size(), test.size()) 
                 self.x_t_latent_buffer = (mask) * self.x_t_latent_buffer + (1 - mask) * test
             else:
                 x_0_pred_out = x_0_pred_batch
